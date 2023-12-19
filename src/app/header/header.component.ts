f9b9acb6aa9e911069476f1acc86df47c07716c3
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-12-19 10:40:30 +0100

    scroll tab-stops

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -20,7 +20,7 @@ export class HeaderComponent {
    */
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.checkElementVisibility('about-tab-stop', -300, window.innerHeight+300, 'hasReachedAbout');
+    this.checkElementVisibility('about-tab-stop', -300, window.innerHeight-600, 'hasReachedAbout');
     this.checkElementVisibility('skills-tab-stop', -200, window.innerHeight-500, 'hasReachedSkills');
     this.checkElementVisibility('portfolio-tab-stop', -600, window.innerHeight-600, 'hasReachedPortfolio');
   }

9c0fc611f537eefafbc76841178c563ac1cc84cf
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-12-19 12:08:14 +0100

    slide in function

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -7,7 +7,55 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-  constructor(public translate: TranslateService) {
-    
+
+  /**
+   * Flag indicating if the according section is in view.
+   */
+  hasReachedAbout = false;
+  hasReachedSkills = false;
+  hasReachedPortfolio = false;
+
+  /**
+   * Listens for the window scroll event and updates visibility flags for specific sections.
+   */
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkElementVisibility('portfolio-tab-stop', -600, window.innerHeight-600, 'hasReachedPortfolio');
   }
+
+  /**
+   * Checks the visibility of a specific element on the page.
+   * @param elementId - The ID of the element to check.
+   * @param topOffset - The top offset for visibility check.
+   * @param bottomOffset - The bottom offset for visibility check.
+   * @param propertyToUpdate - The property to update based on the element's visibility.
+   */
+  private checkElementVisibility(elementId: string, topOffset: number, bottomOffset: number, propertyToUpdate: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      if (rect.top >= topOffset && rect.bottom <= bottomOffset) {
+        this[propertyToUpdate] = true;
+      }
+    }
+  }
+
+  /**
+   * Input property to determine whether to show the menu.
+   */
+  @Input() showMenu: boolean = false;
+
+  /**
+   * Event emitter to notify the parent component about the change in the menu status.
+   */
+  @Output() changeMenu = new EventEmitter<boolean>();
+
+  /**
+   * Emits an event to change the menu status.
+   */
+  changeMenuFunction(){
+    this.changeMenu.emit(true);
+  }
+
+  constructor(public translate: TranslateService) {}
 }
